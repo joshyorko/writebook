31fdd78ffc7a446e5d0eb2e3479aac36ee8b8ffe
Alberto Fernández-Capel <afcapel@gmail.com>
2024-04-26 11:25:18 +0100

    Updated toolbar

--- app/helpers/house_helper.rb
+++ app/helpers/house_helper.rb
@@ -7,7 +7,16 @@ module HouseHelper
     tag.house_md_toolbar **options, &block
   end
 
-  def house_button(action, **options, &block)
+  def house_toolbar_button(action, **options, &block)
     tag.button title: action.to_s.humanize, data: { "house-md-action": action }, **options, &block
+  end
+
+  def house_toolbar_file_upload_button(title = "Upload File", **options, &block)
+    tag.label title: title, **options do
+      safe_join [
+        file_field_tag("test", data: { "house-md-toolbar-file-picker": true }, style: "display: none;"),
+        capture(&block)
+      ]
+    end
   end
 end
