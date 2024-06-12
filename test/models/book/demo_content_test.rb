0f49e89d5c97f6469431ec2a9510634c3274cbf2
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-06-12 17:31:35 -0500

    Skip this test for now, we may be dumping this feature

--- test/models/book/demo_content_test.rb
+++ test/models/book/demo_content_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class Book::DemoContentTest < ActiveSupport::TestCase
   test "new books have demo content when created" do
+    skip "Until we decide if we're keeping this feature."
     book = Book.create! title: "New Book"
 
     assert book.leaves.pages.count == 1
