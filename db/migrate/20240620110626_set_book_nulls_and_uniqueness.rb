400f6ea4833292e661d81745e6b0a4cc78591085
Kevin McConnell <kevin@37signals.com>
2024-06-20 16:07:48 +0100

    Use a slightly less ugly placeholder for slugs
    We'll remove this before launch, so it's just about improving the impact
    it has on existing test data.

--- db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
+++ db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
@@ -1,6 +1,6 @@
 class SetBookNullsAndUniqueness < ActiveRecord::Migration[8.0]
   def change
-    execute "update books set slug = id where slug is null"
+    execute "update books set slug = 'book' where slug is null"
 
     change_column_null :books, :title, false
     change_column_null :books, :slug, false
