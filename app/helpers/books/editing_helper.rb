615c25e9b7bb9605d231ec30f81f88f48a785525
Kevin McConnell <kevin@37signals.com>
2024-06-20 14:47:52 +0100

    Use book_leafable_path when reading

--- app/helpers/books/editing_helper.rb
+++ app/helpers/books/editing_helper.rb
@@ -1,6 +1,6 @@
 module Books::EditingHelper
   def editing_mode_toggle_switch(leaf, checked:)
-    target_url = checked ? leafable_path(leaf) : edit_leafable_path(leaf)
+    target_url = checked ? book_leafable_path(leaf.book, leaf) : edit_leafable_path(leaf)
     render "books/edit_mode", target_url: target_url, checked: checked
   end
 end
