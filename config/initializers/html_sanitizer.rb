6406912d1f83079a72761a0a20db5c400879a22d
Kevin McConnell <kevin@37signals.com>
2024-06-20 10:41:27 +0100

    Allow iframes in page content

--- config/initializers/html_sanitizer.rb
+++ config/initializers/html_sanitizer.rb
@@ -1,5 +1,5 @@
 Rails.application.config.action_view.sanitized_allowed_tags =
-  Rails::HTML5::SafeListSanitizer.allowed_tags + %w[ center details summary table tbody td tfoot th thead tr video ]
+  Rails::HTML5::SafeListSanitizer.allowed_tags + %w[ center details iframe summary table tbody td tfoot th thead tr video ]
 
 Rails.application.config.action_view.sanitized_allowed_attributes =
   Rails::HTML5::SafeListSanitizer.allowed_attributes + %w[ id style target ]
