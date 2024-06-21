7003faaacf1f7e3e1e06e4303e930674c8c5bde4
Kevin McConnell <kevin@37signals.com>
2024-06-21 14:07:57 +0100

    Ensure old index isn't in the way, on old installs

--- db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
+++ db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
@@ -1,10 +1,9 @@
 class SetBookNullsAndUniqueness < ActiveRecord::Migration[8.0]
   def change
+    remove_index :books, :slug, if_exists: true
     execute "update books set slug = 'book' where slug is null"
 
     change_column_null :books, :title, false
     change_column_null :books, :slug, false
-
-    remove_index :books, :slug
   end
 end
