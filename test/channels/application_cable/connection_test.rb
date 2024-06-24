627219d8552c1ab15eb80e163275b947c5ab741a
Kevin McConnell <kevin@37signals.com>
2024-06-24 14:09:36 +0100

    Add connection test

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,12 +2,18 @@ require "test_helper"
 
 module ApplicationCable
   class ConnectionTest < ActionCable::Connection::TestCase
-    # test "connects with cookies" do
-    #   cookies.signed[:user_id] = 42
-    #
-    #   connect
-    #
-    #   assert_equal connection.user_id, "42"
-    # end
+    test "connects with cookies" do
+      users(:kevin).sessions.start!(user_agent: "test", ip_address: "10.0.0.1").tap do |session|
+        cookies.signed[:session_token] = session.token
+      end
+
+      connect
+
+      assert_equal users(:kevin), connection.current_user
+    end
+
+    test "rejects connection without cookies" do
+      assert_reject_connection { connect }
+    end
   end
 end
