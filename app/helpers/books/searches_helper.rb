be05987619cb0ba36d4d79b1cf74c36506e6d6b5
Kevin McConnell <kevin@37signals.com>
2024-10-10 19:25:48 -0400

    Remove unnecessary helpers

--- app/helpers/books/searches_helper.rb
+++
@@ -1,9 +0,0 @@
-module Books::SearchesHelper
-  def highlighted_match(match)
-    sanitize match, tags: %w[ mark ]
-  end
-
-  def search_button(book)
-    render "books/searches/search", book: book
-  end
-end
