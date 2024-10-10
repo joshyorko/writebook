9e90df7fad34af1a3a64f2dd2e54fa062cc7622d
Kevin McConnell <kevin@37signals.com>
2024-10-10 19:23:38 -0400

    Remove early return

--- app/models/leaf/searchable.rb
+++ app/models/leaf/searchable.rb
@@ -12,14 +12,12 @@ module Leaf::Searchable
   class_methods do
     def search(terms)
       terms = sanitize_query_syntax(terms)
-      return none unless terms.present?
 
-      joins("join leaf_search_index on leaves.id = leaf_search_index.rowid")
-        .where("leaf_search_index match ?", terms)
-        .select(
-          "leaves.*",
-          "highlight(leaf_search_index, 0, '<mark>', '</mark>') as title_match",
-          "snippet(leaf_search_index, 1, '<mark>', '</mark>', '...', 20) as content_match")
+      if terms.present?
+        perform_search(terms)
+      else
+        none
+      end
     end
 
     def reindex_all
@@ -31,6 +29,15 @@ module Leaf::Searchable
         terms = remove_invalid_search_characters(terms)
         terms = remove_unbalanced_quotes(terms)
         terms
+      end
+
+      def perform_search(terms)
+        joins("join leaf_search_index on leaves.id = leaf_search_index.rowid")
+          .where("leaf_search_index match ?", terms)
+          .select(
+            "leaves.*",
+            "highlight(leaf_search_index, 0, '<mark>', '</mark>') as title_match",
+            "snippet(leaf_search_index, 1, '<mark>', '</mark>', '...', 20) as content_match")
       end
 
       def remove_invalid_search_characters(terms)
