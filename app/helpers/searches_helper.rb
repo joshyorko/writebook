d257a388cdc6adcc548e217c908cf431a77f4f0d
Kevin McConnell <kevin@37signals.com>
2024-10-29 08:42:00 +0000

    Only highlight whole word matches

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,9 +2,15 @@ module SearchesHelper
   def highlight_page_body(page, query)
     if query.present?
       terms = page.leaf.matches_for_highlight(query)
+      terms = whole_word_matchers(terms)
       sanitize_content highlight(page.body.to_html, terms, sanitize: false)
     else
       sanitize_content page.body.to_html
     end
   end
+
+  private
+    def whole_word_matchers(terms)
+      terms.map { |term| /\b#{term}\b/ }
+    end
 end
